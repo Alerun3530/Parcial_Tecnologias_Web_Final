61975d7edb947d9566c812f58b4ac453e5d602e3
Yuderlis Toro <alerun3520@gmail.com>
2026-04-30 20:34:55 -0500

    controlador CRUD de estudiantes actualizado

--- backend/controllers/student_controller.py
+++ backend/controllers/student_controller.py
@@ -1,26 +1,49 @@
-from fastapi import HTTPException, Depends
-from models.student_model import Student
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from models.db_model import Students as StudentDB
-from database import get_db
+
+from backend.models.db_model import Students as StudentDB
+from backend.models.student_model import Student
+
 
 class StudentController:
     @staticmethod
-    def get_all(db: Session=Depends(get_db)) -> list[dict]:
-        return db.query(StudentDB).all()
-    
-    @staticmethod
-    def get_by_id(student_id: int, db: Session= Depends(get_db))->dict:
-        student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
+    def get_all(db: Session) -> list:
+        return db.query(StudentDB).order_by(StudentDB.id.asc()).all()
 
+    @staticmethod
+    def get_by_id(student_id: int, db: Session) -> dict:
+        student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
         if not student:
             raise HTTPException(status_code=404, detail="Estudiante no encontrado")
         return student
-    
+
     @staticmethod
-    def create(student: Student, db: Session= Depends(get_db))->dict:
+    def create(student: Student, db: Session) -> dict:
         new_student = StudentDB(**student.model_dump())
         db.add(new_student)
         db.commit()
         db.refresh(new_student)
         return new_student
+
+    @staticmethod
+    def update(student_id: int, update_data: Student, db: Session) -> dict:
+        student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
+        if not student:
+            raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+
+        for field, value in update_data.model_dump().items():
+            setattr(student, field, value)
+
+        db.commit()
+        db.refresh(student)
+        return student
+
+    @staticmethod
+    def delete(student_id: int, db: Session) -> dict:
+        student = db.query(StudentDB).filter(StudentDB.id == student_id).first()
+        if not student:
+            raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+
+        db.delete(student)
+        db.commit()
+        return {"message": f"Estudiante {student_id} eliminado"}
