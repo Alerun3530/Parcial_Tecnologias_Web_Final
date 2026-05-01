9013fae62668091d02c88ae1dde6bb3776d76731
Yuderlis Toro <alerun3520@gmail.com>
2026-04-30 20:31:58 -0500

    Agregar apellido a estudiante

--- backend/models/student_model.py
+++ backend/models/student_model.py
@@ -2,8 +2,19 @@ from pydantic import BaseModel, Field
 
 class Student(BaseModel):
     name: str = Field(..., min_length=2)
-    age: int = Field(...,gt=0)
-    grade: float=Field(...,ge=0, le=5)
+    lastname: str = Field(..., min_length=2) 
+    age: int = Field(..., gt=0)
+    grade: float = Field(..., ge=0, le=5)
 
 class StudentResponse(Student):
-    id: int
+    id: int  
+
+    class Config:
+        from_attributes = True
+
+class EmailRequest(BaseModel):
+    email: str
+
+class OTPVerify(BaseModel):
+    email: str
+    code: str
